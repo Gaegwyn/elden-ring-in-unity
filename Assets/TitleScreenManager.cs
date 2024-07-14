02554ec698d0b5e23ef887ef0142c2b37809d27f
Gaegwyn <gaegxo@gmail.com>
2024-07-14 14:49:00 -0500

    Added more world managers and main scene

--- Assets/TitleScreenManager.cs
+++ Assets/TitleScreenManager.cs
@@ -8,5 +8,10 @@ public class TitleScreenManager : MonoBehaviour
     public void StartNetworkAsHost()
     {
         NetworkManager.Singleton.StartHost();
-    }    
+    }
+
+    public void StartNewGame()
+    {
+        StartCoroutine(WorldSaveGameManager.Instance.LoadNewGame());
+    }
 }
---
+++ Assets/WorldSaveGameManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WorldSaveGameManager : MonoBehaviour
+{
+    public static WorldSaveGameManager Instance;
+
+    [SerializeField] int WorldSceneIndex = 1;
+
+    private void Awake()
+    {
+        // Singleton
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public IEnumerator LoadNewGame()
+    {
+        AsyncOperation LoadOperation = SceneManager.LoadSceneAsync(WorldSceneIndex);
+
+        yield return null;
+    }
+}
